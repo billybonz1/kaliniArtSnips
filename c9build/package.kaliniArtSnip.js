53f33e3dd35bc530f687f08588e037963127b4d5
Artem <kalini.art@gmail.com>
2019-07-11 03:47:42 +0000

    3

--- c9build/package.kaliniArtSnip.js
+++ c9build/package.kaliniArtSnip.js
@@ -25,7 +25,7 @@ define("plugins/kaliniArtSnip/__static__",[], function(require, exports, module)
                 {
                     "type": "snippets",
                     "filename": "php.snippets",
-                    "data": "# scope: php\nsnippet prr\n\techo \"<pre>\";"
+                    "data": "# scope: php\nsnippet test\n\tif (${1:true}) {\n\t\t${2}\n\t}\n\t$0"
                 }
             ].forEach(function(x) {
                 debug.addStaticPlugin(x.type, "kaliniArtSnip", x.filename, x.data, plugin);
